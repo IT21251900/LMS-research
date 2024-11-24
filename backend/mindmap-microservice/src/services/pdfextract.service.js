58c7aec0fcfc6bd399fea56b271bb5bacd921d8b
IT21302862 <sanayasamadhi@gmail.com>
2024-11-24 14:19:50 +0530

    LMS:0004:FIX:MIN:Update the service function to save the whole pdf data into mongoDb directly without define the scheme.
    we don't need to define scheme ,directly saved the collection to db

--- backend/mindmap-microservice/src/services/pdfextract.service.js
+++ backend/mindmap-microservice/src/services/pdfextract.service.js
@@ -1,14 +1,42 @@
 import PdfExtraction from "../schemes/pdfextract.scheme.js";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 class PdfExtractService {
   // Method to save extracted data
+  // async savePdfData(pdfData) {
+  //   try {
+  //     const newPdfExtraction = new PdfExtraction(pdfData);
+  //     await newPdfExtraction.save();
+
+  //     console.log("PDF data saved successfully!");
+  //     return newPdfExtraction;
+  //   } catch (error) {
+  //     console.error("Error saving PDF data:", error);
+  //     throw error;
+  //   }
+  // }
   async savePdfData(pdfData) {
     try {
-      const newPdfExtraction = new PdfExtraction(pdfData);
-      await newPdfExtraction.save();
-
-      console.log("PDF data saved successfully!");
-      return newPdfExtraction;
+      const mongoUri = process.env.MINDMAP_MONGO_URI;
+  
+      if (!mongoUri) {
+        throw new Error("MongoDB URI is not defined in the environment variables.");
+      }
+      const client = await MongoClient.connect(mongoUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      
+      const db = client.db(); 
+      const collection = db.collection("Pdfextractions"); 
+      const result = await collection.insertOne(pdfData);
+  
+      console.log("PDF data saved successfully!", result);
+      client.close();
+      return result;
     } catch (error) {
       console.error("Error saving PDF data:", error);
       throw error;
