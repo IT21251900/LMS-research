660010623ef3de1a9bb589079467603653404ab5
Sanjana Dinith <100860580+IT21251900@users.noreply.github.com>
2024-12-04 00:17:10 +0530

    #LMS-fe-UI0033 - Layout | Behaviour Update

--- frontend-v2/src/app/layout/main-dashboard/main-dashboard.component.ts
+++ frontend-v2/src/app/layout/main-dashboard/main-dashboard.component.ts
@@ -23,21 +23,20 @@ import { ReadViewComponent } from "../../components/read-view/read-view.componen
   styleUrls: ["./main-dashboard.component.scss"],
 })
 export class MainDashboardComponent {
-  sidebarWidth = 800; // Initial width of the sidebar
-  splitViewEnabled = true; // Track if the split view is enabled
-  showFirstPanel = true; // Track visibility of the first panel
-  showSecondPanel = true; // Track visibility of the second panel
-  isDragging = false; // Track if the user is dragging
-  initialMouseX = 0; // Store the initial mouse position during drag
+  sidebarWidth = 800;
+  splitViewEnabled = false;
+  showFirstPanel = true;
+  showSecondPanel = true; 
+  isDragging = false; 
+  initialMouseX = 0; 
   title = "miStudy";
   constructor(public router: Router) {}
-  // Toggle the split view on/off
+
   toggleSplitView(): void {
     this.splitViewEnabled = !this.splitViewEnabled;
     this.routeLink("notes");
   }
 
-  // Toggle the visibility of either the first or second panel
   togglePanelVisibility(panel: "first" | "second"): void {
     if (panel === "first") {
       this.showFirstPanel = !this.showFirstPanel;
@@ -46,31 +45,28 @@ export class MainDashboardComponent {
     }
   }
 
-  // When the mouse is pressed down, start dragging
   onMouseDown(event: MouseEvent): void {
     this.isDragging = true;
-    this.initialMouseX = event.clientX; // Store the initial position
-    document.body.style.userSelect = "none"; // Disable text selection during drag
+    this.initialMouseX = event.clientX; 
+    document.body.style.userSelect = "none"; 
   }
 
-  // When mouse is moving, update sidebar width
   @HostListener("document:mousemove", ["$event"])
   onMouseMove(event: MouseEvent): void {
     if (this.isDragging) {
-      const deltaX = event.clientX - this.initialMouseX; // Calculate the difference in X position
+      const deltaX = event.clientX - this.initialMouseX; 
       this.sidebarWidth = Math.max(
         400,
         Math.min(this.sidebarWidth + deltaX, 1800)
-      ); // Constrain the width between 200px and 400px
-      this.initialMouseX = event.clientX; // Update the initial mouse position for the next move
+      ); 
+      this.initialMouseX = event.clientX; 
     }
   }
 
-  // When the mouse is released, stop dragging
   @HostListener("document:mouseup")
   onMouseUp(): void {
     this.isDragging = false;
-    document.body.style.userSelect = ""; // Re-enable text selection
+    document.body.style.userSelect = ""; 
   }
 
   routeLink(type: string) {
